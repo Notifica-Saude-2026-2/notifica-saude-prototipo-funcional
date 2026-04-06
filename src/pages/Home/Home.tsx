bad1abd3ef7837cbc44b7d2d2cfaf5cdc6fa6d46
Luigi Almeida <luigigalmeida@gmail.com>
2026-04-06 12:27:33 -0400

    feat: adiciona pagina inicial de alta fidelidade

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,54 @@
+import styles from "./Home.module.css";
+import { Button } from "../../components/common/ui/Button";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        
+        <h1 className={styles.title}>Sobre</h1>
+
+        <p className={styles.text}>
+          O Notifica Saúde é um sistema destinado ao registro de incidentes e eventos adversos relacionados à assistência à saúde e ao uso de tecnologias como medicamentos e produtos médico-hospitalares.
+        </p>
+
+        <p className={styles.text}>
+          Você pode contribuir informando situações que causaram ou poderiam causar danos à saúde, ajudando a melhorar a qualidade e a segurança dos serviços de saúde no país.
+        </p>
+
+        <div className={styles.actionsContainer}>
+          <span className={styles.actionsTitle}>
+            SELECIONE SUA FORMA DE ACESSO
+          </span>
+
+          <span className={styles.actionsSubtitle}>
+            O que gostaria de fazer?
+          </span>
+
+          <div className={styles.buttons}>
+            <Button
+              variant="outlined"
+              color="orange"
+              fullWidth
+              onClick={() => navigate("/teste")}
+              title="Registrar incidente"
+              subtitle="Não é necessário login. Leva menos de 3 minutos."
+            />
+
+            <Button
+              variant="outlined"
+              color="green"
+              fullWidth
+              title="Acessar área profissional"
+              subtitle="Para gerenciar notificações e relatórios"
+            />
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
