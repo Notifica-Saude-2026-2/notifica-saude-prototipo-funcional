ae268c6eadb51f4da8984359f28e6487f707cb4d
Luigi Almeida <luigigalmeida@gmail.com>
2026-04-06 12:20:57 -0400

    style: altera importacao de layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { Header } from "./components/common/layout/Header";
+import { Footer } from "./components/common/layout/Footer";
+import "./App.css";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <BrowserRouter>
+      <div className="layout">
+        <Header />
+
+        <main className="main">
+          <AppRoutes />
+        </main>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
